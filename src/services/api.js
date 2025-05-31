68d22c0ac23d61aca4e058ba8da99e3737938d4f
Amulya Reddy <samulya2006@gmail.com>
2025-05-31 21:34:08 +0530

    Update OMDB API URL to HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = "d565136f";
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 
 export const searchMovies = async (query, type = "", page = 1) => {
   try {
